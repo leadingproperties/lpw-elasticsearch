60b646346f225e693418c02df2248a9a0c3c6cb5
Alex Baidan <howtwizer@gmail.com>
2016-12-05 13:13:38 +0100

    first commit

---
+++ lib/lpw/elasticsearch.rb
@@ -0,0 +1,7 @@
+require "lpw/elasticsearch/version"
+
+module Lpw
+  module Elasticsearch
+    # Your code goes here...
+  end
+end
---
+++ lib/lpw/elasticsearch/version.rb
@@ -0,0 +1,5 @@
+module Lpw
+  module Elasticsearch
+    VERSION = "0.1.0"
+  end
+end
